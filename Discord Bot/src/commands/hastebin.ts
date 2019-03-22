8e4fb63a1b71de97e5a83cc94ac1047842c93c2b
Tjird <tjeerd@tjird.nl>
2019-03-22 15:17:46 +0100

    Added new command
    Add:
    - Hastebin command, post your code to hastebin and get a link back to the post.

---
+++ Discord Bot/src/commands/hastebin.ts
@@ -0,0 +1,73 @@
+import { IBot, IBotCommand, IBotCommandHelp, IBotMessage, IBotConfig } from '../api'
+import { getRandomInt } from '../utils'
+import * as discord from 'discord.js'
+import BaseCommand from '../baseCommand';
+import * as snekfetch from 'snekfetch';
+
+export default class HastebinCommand extends BaseCommand {
+
+    /**
+     *
+     */
+    constructor() {
+        super(/^\?hastebin/im);
+
+    }
+
+    public getHelp(): IBotCommandHelp {
+        return { caption: '?hastebin', description: 'Posts code to hastebin.' }
+    }
+
+    public init(bot: IBot, dataPath: string): void { }
+
+
+    public canUseInChannel(channel: discord.TextChannel): boolean {
+        return !channel.name.toLowerCase().startsWith("ticket");
+    }
+
+    public async process(msg: string, answer: IBotMessage, message: discord.Message, client: discord.Client, config: IBotConfig, commands: IBotCommand[]): Promise<void> {
+
+        // Split content, take the first part of and take it back together
+        const code = message.content.split(" ").shift().join(" ");
+
+        // Post code to hastebin
+        snekfetch.post('https://hastebin.com/documents')
+            .send(code)
+            .then(res => {
+
+                // Now send the embed
+                this.sendEmbed(res, message);
+            })
+    }
+
+    private sendEmbed(res: any, message: discord.Message) {
+
+        // Create meme embed
+        let hasteEmbed = new discord.RichEmbed()
+
+            // Set the title 
+            .setTitle(`${message.author.username} has posted some code to hastebin`)
+
+            // Set the URL to the hastbin link
+            .setURL(`https://hastebin.com/${res.body.key}`)
+
+            // Set color of embed
+            .setColor("#ff0000")
+
+            // Set the description of the embed
+            .setDescription(`Here is the URL to your hastebin link.\nhttps://hastebin.com/${res.body.key}`)
+
+            // Set timestamp to current time
+            .setTimestamp()
+
+            // Set footer to 'posted by mickie456' if author was mickie456
+            .setFooter(`posted by ${post.data.author}`);
+
+        // Send embed
+        message.channel.send(hasteEmbed);
+
+        // Remove the original message
+        message.delete();
+    }
+
+}
