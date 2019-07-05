22626f928d1e00a1d3bf0791afca75d8022ff628
mickie456 <rustenhovenmick@gmail.com>
2019-07-05 23:20:56 +0000

    prefix fix, proficiencyCommand fix

--- DiscordBot/src/commands/claimProficienciesCommand.ts
+++ DiscordBot/src/commands/claimProficienciesCommand.ts
@@ -39,7 +39,8 @@ export default class ClaimProficienciesCommand extends BaseCommand {
       "discord server list",
       "youtube member",
       "weapons commissar",
-      "patreon"
+      "patreon",
+      "dapper backup"
     ];
     const toRemove: Discord.Role[] = [];
 
