9727f6eb9a56c938159e38a7419d6296bdafb95c
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2019-08-06 19:00:26 +0100

    Added sync user on guild join and leave

--- DiscordBot/src/events/bot/guildMemberUpdate.ts
+++ DiscordBot/src/events/bot/guildMemberUpdate.ts
@@ -7,12 +7,19 @@ export class GuildMemberUpdateEvent {
   }
 
   private static SyncUser(oldMember: GuildMember, newMember: GuildMember) {
-    if (oldMember === null || newMember === null) { return; }
+
+    let id: string = "";
+
+    if (oldMember !== null) {
+      id = oldMember.id;
+    } else {
+      id = newMember.id;
+    }
 
     new ApiRequestHandler(newMember.client)
       .requestAPI(
         "GET",
         null,
-        `discorduser/syncuser/${newMember.id}`);
+        `discorduser/syncuser/${id}`);
   }
 }
