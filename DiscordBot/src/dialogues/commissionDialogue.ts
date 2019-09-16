85fffc639a77bda1470b5217190283fa8b5fa4dd
Mick Rustenhoven <rustenhovenmick@outlook.com>
2019-09-16 19:18:10 +0200

    Removed some unused commands Fixed commission

--- DiscordBot/src/dialogues/commissionDialogue.ts
+++ DiscordBot/src/dialogues/commissionDialogue.ts
@@ -1,5 +1,11 @@
 import { ValidationError } from "../error";
-import { Message } from "discord.js";
+import { Message, TextChannel, RichEmbed, Client } from "discord.js";
+import { DialogueStep, DialogueHandler } from "../handlers/dialogueHandler";
+import {
+  CommissionFormModel,
+  CommissionForm
+} from "../models/forms/commissionForm";
+import { ApiRequestHandler } from "../handlers/apiRequestHandler";
 
 export class CommissionDialogue {
   public nameStep(response: Message, data: CommissionDialogueData) {
@@ -69,6 +75,99 @@ export class CommissionDialogue {
       } catch (e) {
         return reject(e);
       }
+    });
+  }
+
+  public async CreateDialogue(
+    client: Client,
+    channel: TextChannel,
+    authorId: string
+  ) {
+    let dialogueData = new CommissionDialogueData();
+    let dialogue = new CommissionDialogue();
+
+    let nameStep: DialogueStep<CommissionDialogueData> = new DialogueStep<
+      CommissionDialogueData
+    >(
+      dialogueData,
+      dialogue.nameStep,
+      "Enter the name for your project (max 16 chars)",
+      "Question Successful",
+      "Question Unsuccessful"
+    );
+
+    let descriptionStep: DialogueStep<
+      CommissionDialogueData
+    > = new DialogueStep<CommissionDialogueData>(
+      dialogueData,
+      dialogue.descriptionStep,
+      "Describe your project",
+      "Answer Successful",
+      "Answer Unsuccessful"
+    );
+
+    let functionalitiesStep: DialogueStep<
+      CommissionDialogueData
+    > = new DialogueStep(
+      dialogueData,
+      dialogue.functionalitiesStep,
+      "Describe the functionalities of your project",
+      "URL Choice Successful",
+      "URL Choice Unsuccessful"
+    );
+
+    let budgetStep: DialogueStep<CommissionDialogueData> = new DialogueStep(
+      dialogueData,
+      dialogue.budgetStep,
+      "Provide us some information about the budget for this project",
+      "URL Choice Successful",
+      "URL Choice Unsuccessful"
+    );
+
+    let handler = new DialogueHandler(
+      [nameStep, descriptionStep, functionalitiesStep, budgetStep],
+      dialogueData
+    );
+
+    const member = channel.guild.members.get(authorId);
+
+    if (!member) return;
+
+    await handler.getInput(channel as TextChannel, member.user).then(output => {
+      let commissionForm = new CommissionFormModel();
+
+      commissionForm.name = output.name;
+      commissionForm.description = output.description;
+      commissionForm.functionalitites = output.functionalitites;
+      commissionForm.budget = output.budget;
+
+      commissionForm.discordDiscordId = authorId;
+
+      new ApiRequestHandler(client)
+        .requestAPIWithType<CommissionForm>(
+          "POST",
+          commissionForm,
+          "CommissionForm/Add"
+        )
+        .then(commission => {
+          var embed = new RichEmbed()
+            .setTitle("Your commission has been created!")
+            .addField(
+              "Here you will find the information about your commission",
+              "https://student.dapperdino.co.uk/commission/" + commission.id
+            )
+            .addField("Description:", commission.description)
+            .addField("Status", commission.status)
+            .setFooter("With ❤ by the DapperCoding team")
+            .setColor("FF0000");
+
+          member.send(embed);
+          channel.send(
+            "We've sent you a dm with more information about your request!"
+          );
+          // TODO: Send dm to member & let know in channel
+        })
+        .catch(console.error);
     });
   }
 }
--- DiscordBot/src/helpers/productHelper.ts
+++ DiscordBot/src/helpers/productHelper.ts
@@ -1,143 +1,195 @@
 import { ApiRequestHandler } from "../handlers/apiRequestHandler";
-import { Client, MessageReaction, User, TextChannel, RichEmbed, Message } from "discord.js";
+import {
+  Client,
+  MessageReaction,
+  User,
+  TextChannel,
+  RichEmbed,
+  Message
+} from "discord.js";
 import { RichEmbedReactionHandler } from "../genericRichEmbedReactionHandler";
 import { ClientHelper } from "./clientHelper";
+import { CommissionDialogue } from "../dialogues/commissionDialogue";
 
 export class ProductHelper {
+  public static getProductById(id: number) {}
 
-    public static getProductById(id: number) {
+  public static getProducts(client: Client): Promise<any[]> {
+    return new ApiRequestHandler(client).requestAPIWithType<any[]>(
+      "GET",
+      null,
+      "product"
+    );
+  }
+  public static async createCustomBotReactionHandler(
+    client: Client,
+    userId: string,
+    channel: TextChannel,
+    message: Message | null
+  ) {
+    let embed = new RichEmbed();
 
+    embed.setTitle("DapperCoding Bot Service");
+
+    embed.setDescription(
+      "Are you interested in buying a custom discord bot?\n\nIf you are, react with 👍 or react with 👎 if you are not"
+    );
+    embed.setFooter(
+      `If you want to view all of our current products, react with 🛒`
+    );
+
+    let sent: Message | null = null;
+    if (message != null) {
+      sent = message;
+      await sent.edit(embed);
+      await sent.clearReactions();
+    } else {
+      sent = (await channel.send(embed)) as Message;
     }
-
-    public static getProducts(client: Client): Promise<any[]> {
-
-
-        return new ApiRequestHandler(client).requestAPIWithType<any[]>("GET", null, "product");
-    }
-    public static async createCustomBotReactionHandler(client: Client, userId: string, channel: TextChannel, message:Message|null) {
-        let embed = new RichEmbed();
-
-        embed.setTitle("DapperCoding Bot Service");
-
-        embed.setDescription("Are you interested in buying a custom discord bot?\n\nIf you are, react with 👍 or react with 👎 if you are not");
-        embed.setFooter(`If you want to view all of our current products, react with 🛒`)
-
-        let sent:Message|null =null;
-        if (message != null) {
-            sent = message;
-            await sent.edit(embed);
-            await sent.clearReactions();
+    await sent.react("👍");
+    await sent.react("👎");
+    await sent.react("🛒");
+    sent
+      .awaitReactions(
+        (reaction, user) =>
+          (reaction.emoji.name === "👍" ||
+            reaction.emoji.name == "👎" ||
+            reaction.emoji.name === "🛒") &&
+          user.id === userId,
+        { max: 1 }
+      )
+      .then(collected => {
+        const userReaction = collected.first();
+        const user = this.getUser(userReaction) as User | null;
+        if (!userReaction || !user) return;
+        if (userReaction.emoji.name === "👍") {
+          var dialogue = new CommissionDialogue();
+          if (sent == null) return;
+          dialogue.CreateDialogue(client, sent.channel as TextChannel, userId);
+        } else if (userReaction.emoji.name === "🛒") {
+          const client = ClientHelper.getClient() as Client;
+          ProductHelper.createProductOverviewReactionHandler(
+            client,
+            userId,
+            channel as TextChannel,
+            sent
+          );
         } else {
-            sent = await channel.send(embed) as Message;
+          if (sent) sent.delete();
         }
-            await sent.react("👍");
-            await sent.react("👎");
-            await sent.react("🛒");
-            sent.awaitReactions(
-                (reaction, user) =>
-                    (reaction.emoji.name === '👍' || reaction.emoji.name == '👎' || reaction.emoji.name === '🛒')
-                    && user.id === userId,
-                { max: 1 })
-                .then(collected => {
-                    const userReaction = collected.first();
-                    const user = this.getUser(userReaction) as User | null;
-                    if (!userReaction || !user) return;
-                    if (userReaction.emoji.name === "👍") {
+      });
+  }
 
-                        const requestContactEmbed = new RichEmbed()
-                            .setTitle("A user requested contact about the custom bot service")
-                            .setDescription(`user ${user.username} (${user.id}) requested contact`);
-                        const channel = sent?sent.guild.channels.find(x => x.name.toLowerCase() === "dapper-coding") as TextChannel : null;
+  public static async createProductOverviewReactionHandler(
+    client: Client,
+    userId: string,
+    channel: TextChannel,
+    message: Message | null
+  ) {
+    ProductHelper.getProducts(client).then(async products => {
+      let embed = new RichEmbed();
+      embed.setTitle("Product Catalog");
+      embed.setFooter(
+        "React with one of the numbers to view the product\nClick on '🛒' to view the custom bot service\nOr cancel with '❌'"
+      );
 
-                        if (channel) channel.send(requestContactEmbed);
-                        if(sent) sent.channel.send("Thanks for requesting contact about our custom bot service, we'll inform you asap.")
-                        if (sent) sent.delete();
-                    } else if (userReaction.emoji.name === "🛒") {
-                        const client = ClientHelper.getClient() as Client;
-                        ProductHelper.createProductOverviewReactionHandler(client, userId, channel as TextChannel, sent);
-                    } else {
-                        if (sent) sent.delete();
-                    }
-                    
-                })
-    }
+      var reaction_numbers = [
+        "\u0031\u20E3",
+        "\u0032\u20E3",
+        "\u0033\u20E3",
+        "\u0034\u20E3",
+        "\u0035\u20E3",
+        "\u0036\u20E3",
+        "\u0037\u20E3",
+        "\u0038\u20E3",
+        "\u0039\u20E3"
+      ];
+      let getNumberForEmoji = (emoji: string): number => {
+        for (let i = 0; i < reaction_numbers.length; i++) {
+          if (getEmojiForNumber(i) === emoji) {
+            return i;
+          }
+        }
+        return -1;
+      };
+      let getEmojiForNumber = (i: number): string => {
+        return reaction_numbers[i];
+      };
+      let mapped = products.map((v, index) =>
+        getEmojiForNumber(index)
+      ) as string[];
+      let emojis = ["🛒", ...mapped, "❌"];
 
-    public static async createProductOverviewReactionHandler(client: Client, userId: string, channel: TextChannel, message:Message|null) {
-        ProductHelper.getProducts(client).then(async products => {
-            let embed = new RichEmbed();
-            embed.setTitle("Product Catalog");
-            embed.setFooter("React with one of the numbers to view the product\nClick on '🛒' to view the custom bot service\nOr cancel with '❌'");
-            
+      products.forEach((product, index) => {
+        embed.addField(`#${index + 1} ${product.name}`, "$" + product.price);
+      });
 
-            var reaction_numbers = [
-                "\u0031\u20E3",
-                "\u0032\u20E3",
-                "\u0033\u20E3",
-                "\u0034\u20E3",
-                "\u0035\u20E3",
-                "\u0036\u20E3",
-                "\u0037\u20E3",
-                "\u0038\u20E3",
-                "\u0039\u20E3"
-            ];
-            let getNumberForEmoji = (emoji: string): number => {
-                for (let i = 0; i < reaction_numbers.length; i++) {
-                    if (getEmojiForNumber(i) === emoji) {
-                        return i;
-                    }
-                }
-                return -1;
-            };
-            let getEmojiForNumber = (i: number): string => {
-                return reaction_numbers[i];
-            };
-            let mapped = products.map((v, index) => getEmojiForNumber(index)) as string[];
-            let emojis = ['🛒', ...mapped, '❌'];
+      let sent: Message | null = null;
 
-            products.forEach((product,index)=> {
-                embed.addField(`#${index + 1} ${product.name}`, "$"+product.price)
-            })
+      if (message != null) {
+        sent = message;
+        await sent.edit(embed);
+        await sent.clearReactions();
+      } else {
+        sent = (await channel.send(embed)) as Message;
+      }
 
-            let sent:Message|null = null;
+      for (let i = 0; i < emojis.length; i++) {
+        await sent.react(emojis[i]);
+      }
 
-            if (message != null) {
-                sent = message;
-                await sent.edit(embed);
-                await sent.clearReactions();
-            } else {
-                sent = await channel.send(embed) as Message;
+      sent
+        .awaitReactions(
+          (reaction: MessageReaction, user: User) => {
+            return emojis.includes(reaction.emoji.name) && user.id == userId;
+          },
+          { max: 1 }
+        )
+        .then(collected => {
+          const userReaction = collected
+            .filter(x => x.users.filter(y => y.id == userId).size > 0)
+            .first();
+          const user = userReaction
+            ? userReaction.users.filter(y => y.id == userId).first()
+            : null;
+
+          if (user) {
+            const productIndex = getNumberForEmoji(userReaction.emoji.name);
+            const productId =
+              productIndex >= 0 && productIndex < products.length
+                ? products[productIndex].id
+                : 0;
+            if (productId && productId > 0) {
+              ProductHelper.createProductInfoReactionHandler(
+                productId,
+                client,
+                userId,
+                channel,
+                sent
+              );
+            } else if (userReaction.emoji.name === "🛒") {
+              ProductHelper.createCustomBotReactionHandler(
+                client,
+                userId,
+                channel,
+                sent
+              );
+            } else if (userReaction.emoji.name === "❌") {
+              if (sent) sent.delete();
             }
-
-            for (let i = 0; i< emojis.length; i++) {
-                await sent.react(emojis[i]);
-            }
-
-
-            sent.awaitReactions((reaction: MessageReaction, user: User) => {
-                return (emojis.includes(reaction.emoji.name) && user.id == userId)
-            }, { max: 1 }).then(collected => {
-                const userReaction = collected.filter(x => x.users.filter(y => y.id == userId).size > 0).first();
-                const user = userReaction ? userReaction.users.filter(y => y.id == userId).first() : null;
-
-                if (user) {
-                    const productIndex = getNumberForEmoji(userReaction.emoji.name);
-                    const productId = productIndex >= 0 && productIndex < products.length ? products[productIndex].id : 0;
-                    if (productId && productId > 0) {
-                        ProductHelper.createProductInfoReactionHandler(productId, client, userId, channel, sent)
-                    } else if (userReaction.emoji.name === "🛒") {
-                        ProductHelper.createCustomBotReactionHandler(client, userId, channel, sent);
-                    } else if (userReaction.emoji.name === "❌") {
-                        if (sent) sent.delete();
-                    } 
-                    
-                }
-            })
+          }
         });
+    });
+  }
 
-    }
-
-    public static async createProductInfoReactionHandler(id:number, client:Client,userId:string, channel:TextChannel, message:Message|null) {
-        new ApiRequestHandler(client)
+  public static async createProductInfoReactionHandler(
+    id: number,
+    client: Client,
+    userId: string,
+    channel: TextChannel,
+    message: Message | null
+  ) {
+    new ApiRequestHandler(client)
       .requestAPIWithType<any>("GET", null, "product/" + id)
       .then(async product => {
         const embed = new RichEmbed()
@@ -146,7 +198,9 @@ export class ProductHelper {
           )
           .setDescription(product.description)
           .setURL("https://dapperdino.co.uk/Products/Information/" + product.id)
-          .setFooter("Click on the title to open in your web browser\nReact with '🛒' to view the overview of products\nOr react with '❌' to cancel");
+          .setFooter(
+            "Click on the title to open in your web browser\nReact with '🛒' to view the overview of products\nOr react with '❌' to cancel"
+          );
 
         if (product.instructions) {
           embed.addField(
@@ -174,48 +228,67 @@ export class ProductHelper {
           }
         }
 
-        let sent:Message|null =null;
+        let sent: Message | null = null;
         if (message != null) {
-            sent = message;
-            await message.edit(embed);
-            await message.clearReactions();
+          sent = message;
+          await message.edit(embed);
+          await message.clearReactions();
         } else {
-            sent = await channel.send(embed) as Message;
+          sent = (await channel.send(embed)) as Message;
         }
 
         await sent.react("🛒");
         await sent.react("❌");
 
-        sent.awaitReactions((reaction: MessageReaction, user: User) => ((reaction.emoji.name === "🛒" || reaction.emoji.name === "❌") && user.id == userId), { max: 1 }).then(collected => {
-            const userReaction = collected.filter(x => x.users.filter(y => y.id == userId).size > 0).first();
-            const user = userReaction ? userReaction.users.find(y => y.id == userId) : null;
+        sent
+          .awaitReactions(
+            (reaction: MessageReaction, user: User) =>
+              (reaction.emoji.name === "🛒" || reaction.emoji.name === "❌") &&
+              user.id == userId,
+            { max: 1 }
+          )
+          .then(collected => {
+            const userReaction = collected
+              .filter(x => x.users.filter(y => y.id == userId).size > 0)
+              .first();
+            const user = userReaction
+              ? userReaction.users.find(y => y.id == userId)
+              : null;
 
-           if (user) {
-                if (userReaction.emoji.name === "🛒") {
-                    ProductHelper.createProductOverviewReactionHandler(client, userId, channel, sent);
-                } else {
-                    if (sent) sent.delete();   
-                }
-           }
-        })
+            if (user) {
+              if (userReaction.emoji.name === "🛒") {
+                ProductHelper.createProductOverviewReactionHandler(
+                  client,
+                  userId,
+                  channel,
+                  sent
+                );
+              } else {
+                if (sent) sent.delete();
+              }
+            }
+          });
       })
       .catch(err => {
-          channel.send("Something went wrong, please try again later");
-          if(message) message.delete();
+        channel.send("Something went wrong, please try again later");
+        if (message) message.delete();
 
-          const teamChannel = channel.guild.channels.find(x=>x.name.toLowerCase() === "dapper-team") as TextChannel;
-          
-          teamChannel.send(`Error occured getting product information for id ${id} <@211115689807839232>`);
-          console.error(err);
-        });
-    }
+        const teamChannel = channel.guild.channels.find(
+          x => x.name.toLowerCase() === "dapper-team"
+        ) as TextChannel;
 
-    private static getUser = (userReaction: MessageReaction) => {
-        let retUser: User | null = null;
-        userReaction.users.forEach(user => {
-            if (!user.bot)
-                retUser = user;
-        });
-        return retUser;
-    }
+        teamChannel.send(
+          `Error occured getting product information for id ${id} <@211115689807839232>`
+        );
+        console.error(err);
+      });
+  }
+
+  private static getUser = (userReaction: MessageReaction) => {
+    let retUser: User | null = null;
+    userReaction.users.forEach(user => {
+      if (!user.bot) retUser = user;
+    });
+    return retUser;
+  };
 }
