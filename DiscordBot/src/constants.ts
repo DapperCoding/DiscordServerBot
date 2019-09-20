23de9c3f51b320f5d147e0006d5b948eeedf0b89
mickie456 <rustenhovenmick@gmail.com>
2019-09-20 10:47:04 +0000

    Constants fix

--- DiscordBot/src/constants.ts
+++ DiscordBot/src/constants.ts
@@ -1,7 +1,9 @@
 export class Constants {
-  public static EmbedColors: {
-    YELLOW: [241, 196, 15];
-    GREEN: [0, 255, 0];
-    RED: [255, 0, 0];
+  public static readonly EmbedColors= {
+    YELLOW: [241, 196, 15] as RGB,
+    GREEN: [0, 255, 0] as RGB,
+    RED: [255, 0, 0] as RGB
   };
 }
+
+type RGB = [number,number,number];
