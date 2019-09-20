c76e962b982fd79c1111f73fb30407a52c71cd51
Mick Rustenhoven <rustenhovenmick@outlook.com>
2019-09-20 12:52:32 +0200

    formbase validation fixes

--- DiscordBot/src/dialogues/FormBaseDialogue.ts
+++ DiscordBot/src/dialogues/FormBaseDialogue.ts
@@ -18,6 +18,10 @@ export class FormBaseDialogue<T extends FormBaseDialogueData> {
           reject(new ValidationError("You must enter a number."));
         }
 
+        if (data.age < 13) {
+          reject(new ValidationError("You must be at least 13 years old."));
+        }
+
         return resolve(data);
       } catch (e) {
         return reject(e);
@@ -28,10 +32,10 @@ export class FormBaseDialogue<T extends FormBaseDialogueData> {
   public motivationStep(response: Discord.Message, data: T) {
     return new Promise<T>((resolve, reject) => {
       try {
-        if (response.content.length <= 60) {
+        if (response.content.length < 80 || response.content.length > 400) {
           reject(
             new ValidationError(
-              "You must provide a 60-200 character message on what motivates you."
+              "You must provide a 80-400 character message on what motivates you."
             )
           );
         }
@@ -47,10 +51,10 @@ export class FormBaseDialogue<T extends FormBaseDialogueData> {
   public developmentExperience(response: Discord.Message, data: T) {
     return new Promise<T>((resolve, reject) => {
       try {
-        if (response.content.length <= 60) {
+        if (response.content.length < 80 || response.content.length > 400) {
           reject(
             new ValidationError(
-              "You must provide a 60-200 character message on what development experience you have."
+              "You must provide a 80-400 character message on what development experience you have."
             )
           );
         }
