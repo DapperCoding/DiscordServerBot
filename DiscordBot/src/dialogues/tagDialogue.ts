a3ba79d4b2ebbe6841507d2fbac636ca863da675
KennyThouSinner <odeukuracsve@gmail.com>
2019-10-21 15:45:52 +0200

    Create the tag dialogue itself

---
+++ DiscordBot/src/dialogues/tagDialogue.ts
@@ -0,0 +1,47 @@
+import * as Discord from 'discord.js';
+import { BaseDialogueData, BaseDialogue } from './baseDialogue';
+import { DialogueHandler, DialogueStep } from '../handlers/dialogueHandler';
+import { TagBaseDialogue } from './TagBaseDialogue';
+
+export class TagDialogue extends TagBaseDialogue<TagDialogueData> implements BaseDialogue<TagDialogueData>{
+
+    public async createHandler(
+        data: TagDialogueData,
+        channel: Discord.TextChannel,
+        user: Discord.User,
+        callback: (data: TagDialogueData) => void
+    ) {
+        let nameStep: DialogueStep<
+            TagDialogueData
+        > = new DialogueStep<TagDialogueData>(
+            data,
+            this.nameStep,
+            "Which name do you wish to cast on this tag?"
+        );
+
+        let descStep: DialogueStep<
+            TagDialogueData
+        > = new DialogueStep<TagDialogueData>(
+            data,
+            this.descStep,
+            "Which description do you wish to cast on this tag?"
+        );
+
+        let handler = new DialogueHandler(
+            [nameStep, descStep],
+            data
+        );
+
+        await handler 
+        .getInput(
+            channel,
+            user
+        )
+        .then(callback);
+    }
+}
+
+export interface TagDialogueData extends BaseDialogueData {
+    name: string;
+    answer: string;
+}
